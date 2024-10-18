613fc8ed82bd88b7b4aec34e7eb19d0bba79bb38
Abhesh Mandal <abheshmandal249@gmail.com>
2024-10-18 21:53:06 +0545

    db creation and table creation

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,16 +1,24 @@
-import { Sequelize} from "sequelize-typescript";
-import { envConfig } from "../config/config";
-const sequelize = new Sequelize(envConfig.connectionstring as string );
+import {Sequelize} from 'sequelize-typescript'
+import { envConfig } from '../config/config'
+
+const sequelize = new Sequelize(envConfig.connectionstring as string,{
+    models : [__dirname + '/models']
+})
 
 try {
     sequelize.authenticate()
     .then(()=>{
-        console.log("milyo hai db ko Authentication!!!!")
+        console.log("milyo hai authentication so ma connect vaye hai tw Db mah !!!")
     })
     .catch(err=>{
-        console.log("error aayo hai !!!",err)
+        console.log("error aayo", err)
     })
 } catch (error) {
-   console.log(error); 
+    console.log(error)
 }
+
+sequelize.sync({force : false}).then(()=>{
+    console.log("local changes injected to database successfully")
+})
+
 export default sequelize
