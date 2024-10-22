484263064438517270cd2c9e0997f04dddb77167
Abhesh Mandal <abheshmandal249@gmail.com>
2024-10-22 16:06:57 +0545

    Basic Login Finsh Done

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,22 +3,22 @@ import User from "../database/models/userModel";
 import bcrypt from "bcrypt";
 
 
-class UserController{
-    static async register(req:Request,res:Response){
+class UserController {
+    static async register(req: Request, res: Response) {
         //incoming user data receive 
-        const {username,email,password} = req.body
-        if(!username || !email || !password){
+        const { username, email, password } = req.body
+        if (!username || !email || !password) {
             res.status(400).json({
-                message : "Please provide username,email,password"
+                message: "Please provide username,email,password"
             })
             return
         }
         // data --> users table ma insert garne 
         await User.create({
-            username, 
-            email, 
-            password : bcrypt.hashSync(password,10), 
-    
+            username,
+            email,
+            password: bcrypt.hashSync(password, 10),
+
         })
 
         // await sequelize.query(`INSERT INTO users(id,username,email,password) VALUES (?,?,?,?)`, {
@@ -26,9 +26,47 @@ class UserController{
         // })
 
         res.status(201).json({
-            message : "User registered successfully"
+            message: "User registered successfully"
         })
     }
+  static  async login(req: Request, res: Response) {
+        //accept the incoming data ---> email and password from the users
+        const { email, password } = req.body // abhesh ---> Sending through Plain text ---> hash() 343ifuyd4u58
+        if (!email || !password) {
+            res.status(400).json({
+                message: "PLelase provide the email and password😢"
+            })
+        }
+        return
+
+        const [user] = await User.findAll({ // find = findAll --> return data in an array , findById = findByPk---> OBJECT
+            where: {
+                email: email
+            }
+        })
+        // passwor are stored in hash form Anhesh ----> 343ifuyd4u58
+        // IF yes then check password too
+        if (!user) {
+            res.status(404).json({
+                message: "No useer with that email😢"
+            })
+        } else {
+            // IF yes--> email exits then check password too
+            const isEqual = bcrypt.compareSync(password, user.password)
+            if (!isEqual) {
+                res.status(404).json({
+                    message: "Invalid Password From the user😢"
+                })
+            }else{
+                // if Password milyo vnney --> JWT token
+                res.status(200).json({
+                    message : "Logged in Sucessfully❤😍"
+                })
+            }
+     }
+
+    }
+
 }
 
 
