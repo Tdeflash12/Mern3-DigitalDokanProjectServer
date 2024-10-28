411536a4d3b09444c8b05e349995e201d93697bc
Abhesh Mandal <abheshmandal249@gmail.com>
2024-10-28 21:33:30 +0545

    Reset password using OTP

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,11 +1,15 @@
-import express from 'express';
-import UserController from '../controllers/userController';
 
-const router = express.Router();
+import express from 'express'
+import UserController from '../controllers/userController'
+const router = express.Router()
 
-// Define routes
-router.route("/register").post(UserController.register);
-router.route("/login").post(UserController.login);
-router.route("/forgot-password").post(UserController.handleForgotPassword);
+// router.post("/register",UserController.register)
+// router.get("/register",UserController.register)
 
-export default router;
+router.route("/register").post(UserController.register)
+router.route("/login").post(UserController.login)
+router.route("/forgot-password").post(UserController.handleForgotPassword)
+router.route("/verify-otp").post(UserController.verifyOtp)
+router.route("/reset-password").post(UserController.resetPassword)
+
+export default router 
